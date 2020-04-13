9789960ddf926886d85464dd2ead53381dcea9e3
Benitez Fernando <49913532+benitezfernando@users.noreply.github.com>
2020-04-13 17:47:42 -0300

    Update

--- js/jugador.js
+++ js/jugador.js
@@ -1,8 +1,6 @@
-/* El objeto jugador es un objeto literal que se encuentra incompleto.
- Solo tiene asignadas algunas de sus propiedades y ningun metodo */
+
 var Jugador = {
-  /* el sprite contiene la ruta de la imagen
-   */
+  
   sprite: "imagenes/auto_rojo_abajo.png",
   x: 130,
   y: 160,
@@ -10,7 +8,7 @@ var Jugador = {
   alto: 30,
   velocidad: 10,
   vidas: 5,
-  // Hay que agregar lo que falte al jugador: movimientos, perdida de vidas,
+  
   movimiento: function(direccion) {
     if (direccion == "izq") {
       this.x -= 10;
