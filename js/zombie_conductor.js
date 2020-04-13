210104e7af679aa676ab2a65a043f739af1edbff
Benitez Fernando <49913532+benitezfernando@users.noreply.github.com>
2020-04-13 17:50:30 -0300

    Update

--- js/zombie_conductor.js
+++ js/zombie_conductor.js
@@ -1,8 +1,4 @@
-/* Para insipirarte para la implementacion del ZombieConductor podes usar
-al ZombieCaminante de ejemplo. Tene en cuenta que tendra algunas diferencias.
-Por ejemplo, la cantidad parametros que recibe su constructor. En ZombieConductor
-no son exactamente los mismos parametros que en el objeto Enemigo, a diferencia
-del ZombieCaminante que eran los mismos. */
+
 
 var ZombieConductor = function(
   sprite,
@@ -14,17 +10,17 @@ var ZombieConductor = function(
   rangoMov,
   direccion
 ) {
-  /* Completar constructor a partir de Enemigo */
+  
   this.direccion = direccion;
   Enemigo.call(this, sprite, x, y, ancho, alto, velocidad, rangoMov);
-  /* No olvidar agregar la/s propiedad/es unicas de ZombieConductor necesarias */
+  
 };
 
-/* Completar creacion del ZombieConductor */
+
 ZombieConductor.prototype = Object.create(Enemigo.prototype);
 ZombieConductor.prototype.constructor = ZombieConductor;
 
-/* Completar metodos para el movimiento y el ataque */
+
 ZombieConductor.prototype.mover = function() {
   if (this.direccion === "h") {
     this.x -= this.velocidad;
@@ -35,7 +31,7 @@ ZombieConductor.prototype.mover = function() {
   if (this.x < this.rangoMov.desdeX || this.x > this.rangoMov.hastaX) {
     this.velocidad *= -1;
   }
-  // Si sobrepasa el rangoY, lo manda al centro entre ambos rangos
+ 
   if (this.y < this.rangoMov.desdeY || this.y > this.rangoMov.hastaY) {
     this.velocidad *= -1;
   }
